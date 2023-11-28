c3b0d7568caf6e953754e9288d5cafa5847770ce
João Netto <jovinettonetto@gmail.com>
2023-11-28 13:29:41 +0000

    codigo comentado

--- app/src/main/java/application/model/User.java
+++ app/src/main/java/application/model/User.java
@@ -6,33 +6,36 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "users")
+@Entity // Indica que esta classe é uma tabela do banco
+@Table(name = "users") // Especifica a tabela do banco de dados
+
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    private String username;
-    private String password;
+    @Id // Indica que o campo abaixo é a chave primária da tabela
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Define a estratégia de geração de valor para a chave primária
+    private long id; // Campo representando a chave primária da tabela
+
+    private String username; // Campo para armazenar o nome de usuário
+    private String password; // Campo para armazenar a senha do usuário
+
+    // Gets e Sets para acessar e modificar os campos privados
 
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getPassword() {
         return password;
     }
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
-
-
